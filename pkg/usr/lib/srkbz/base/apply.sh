2841c446caf41166546f2f65664f8fb1d7a1aef6
Carlos Fdez. Llamas <hello@sirikon.me>
2020-06-17 23:12:25 +0200

    Dependencies and scripts

---
+++ pkg/usr/lib/srkbz/base/apply.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(realpath $(dirname ${BASH_SOURCE[0]}))"
+
+CONFIG_PATH="/etc/srkbz/config-base.env"
+
+function main {
+    load-config
+
+    apply-ufw \
+        "default allow outgoing" \
+        "default deny incoming" \
+        "allow in 22 comment SSH" \
+        "allow in 80 comment Caddy-HTTP" \
+        "allow in 443 comment Caddy-HTTPS" \
+        "allow in 25565 comment Minecraft" \
+
+    configure-caddy
+}
+
+function apply-ufw {(
+    log-title "Applying UFW configuration"
+    run-silent ufw --force reset
+    rm /etc/ufw/*rules.*
+    for rule in "$@"
+    do
+        run-silent ufw $rule
+    done
+    run-silent ufw --force enable
+)}
+
+function configure-caddy {
+    log-title "Configuring Caddy"
+    printf "%s\n" "import sites/*" > "/etc/caddy/Caddyfile"
+    run-silent systemctl reload caddy
+    printf "\n"
+}
+
+function load-config {
+	if [ ! -f "$CONFIG_PATH" ]; then
+		printf "Config file is missing\n"
+		exit 1
+	fi
+	export $(cat ${CONFIG_PATH} | xargs)
+}
+
+function log-title {
+    printf ":: %s\n" "$1"
+}
+
+function run-silent {
+    set +e
+    output=$("$@" 2>&1)
+    result=$?
+    set -e
+    if [ "$result" -gt "0" ]; then
+        log-error "Error while running command:"
+        log-error "$*"
+        printf "%s\n" "$output"
+        exit "$result"
+    fi
+}
+
+main "$@"
