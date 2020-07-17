017d51b298b688066ab154a63b0d1d0e28d4dfff
Carlos Fdez. Llamas <hello@sirikon.me>
2020-07-17 23:19:33 +0200

    Wait for cloud-init

--- terraform/modules/base/base.tf
+++ terraform/modules/base/base.tf
@@ -28,15 +28,19 @@ resource "hcloud_server" "server" {
 		host = hcloud_server.server.ipv4_address
 	}
 
+	# Don't know why it's necessary to pre-create the file before
+	# provisioning it.
 	provisioner "remote-exec" {
 		inline = [
 			"mkdir -p /etc/srkbz",
 			"touch /etc/srkbz/config.env"
 		]
 	}
-
 	provisioner "file" {
-		content = "MONITORING_DOMAIN=m-${terraform.workspace}.infra.srk.bz\nMONITORING_SECRET_PATH=${random_string.monitoring-secret.result}\n"
+		content = <<EOF
+MONITORING_DOMAIN=m-${terraform.workspace}.infra.srk.bz
+MONITORING_SECRET_PATH=${random_string.monitoring-secret.result}
+EOF
 		destination = "/etc/srkbz/config.env"
 	}
 }
@@ -55,4 +59,26 @@ resource "hetznerdns_record" "mon-record" {
 	value = "${terraform.workspace}.infra"
 	type = "CNAME"
 	ttl = 60
+}
+
+resource "null_resource" "after-all" {
+	depends_on = [
+		hcloud_server.server,
+		hetznerdns_record.record,
+		hetznerdns_record.mon-record
+	]
+
+	connection {
+		type = "ssh"
+		user = "root"
+		private_key = file("~/.ssh/id_rsa")
+		host = hcloud_server.server.ipv4_address
+	}
+
+	provisioner "remote-exec" {
+		inline = [
+			"while [ ! -f /tmp/cloud-init-done ]; do sleep 2; done",
+			"apt-get install -y srkbz-base"
+		]
+	}
 }
