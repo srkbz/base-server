ae2dc2ba9ee32890bf16b081d52a55e221477b73
Carlos Fdez. Llamas <hello@sirikon.me>
2020-07-24 11:03:30 +0200

    Add basicauth in front of ourcraft

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,16 +13,30 @@ provider "hcloud" {
 	token = var.hetzner_cloud_token
 }
 
+resource "random_string" "minecraft-admin-password" {
+  length = 16
+  special = false
+  number = true
+  lower = true
+  upper = false
+}
+
 module "base" {
 	source = "./modules/base"
 
 	target_package = "srkbz-minecraft"
-	config_extra = "MINECRAFT_DOMAIN=${terraform.workspace}.infra.srk.bz\n"
+	config_extra = <<EOF
+MINECRAFT_DOMAIN=${terraform.workspace}.infra.srk.bz
+MINECRAFT_ADMIN_PASSWORD=${base64encode(bcrypt(random_string.minecraft-admin-password.result))}
+EOF
 }
 
+output "minecraft_admin_password" {
+	value = random_string.minecraft-admin-password.result
+}
 output "monitoring_secret" {
-  value = module.base.monitoring_secret
+	value = module.base.monitoring_secret
 }
 output "monitoring_domain" {
-  value = module.base.monitoring_domain
+	value = module.base.monitoring_domain
 }
